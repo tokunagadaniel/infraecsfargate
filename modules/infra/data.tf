a27d5abd03dd7db9184425b8d33239c0ebfa6bc9
tokunagadaniel <tokunagadaniel@gmail.com>
2022-05-01 21:35:34 -0300

    Subindo infra cluster ecs

--- modules/infra/data.tf
+++ modules/infra/data.tf
@@ -1,22 +1,3 @@
-data "aws_caller_identity" "current" {}
-
 data "aws_vpc" "selected" {
   id = var.vpc_id
-}
-
-data "aws_subnet_ids" "private" {
-  vpc_id = var.vpc_id
-}
-
-data "aws_subnet" "private" {
-  for_each = data.aws_subnet_ids.private.ids
-  id       = each.value
-}
-
-data "aws_availability_zones" "zones" {
-  state = "available"
-}
-
-output "subnet_cidr_blocks" {
-  value = [for s in data.aws_subnet.private : s.cidr_block]
 }
